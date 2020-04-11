835fcc825475d1371ed6ce5a400499f05ead8773
ben choe <ben@inlinedigital.io>
2020-04-11 12:21:15 -0400

    ?

--- js/anoesis.js
+++ js/anoesis.js
@@ -5,7 +5,8 @@ let img;
 let vel, pos;
 let xspeed, yspeed;
 
-var images = ['gif/00.gif', 'gif/01.gif', 'gif/02.gif', 'gif/03.gif', 'gif/04.gif', 'gif/05.gif', 'gif/06.gif', 'gif/07.gif', 'gif/08.gif', 'gif/09.gif', 'gif/10.gif', 'gif/11.gif', 'gif/12.gif', 'gif/13.gif', 'gif/14.gif', 'gif/15.gif', 'gif/16.gif', 'gif/17.gif', 'gif/18.gif', 'gif/19.gif', 'gif/20.gif', 'gif/21.gif', 'gif/22.gif', 'gif/23.gif', 'gif/24.gif', 'gif/25.gif', 'gif/26.gif', 'gif/27.gif', 'gif/28.gif', 'gif/29.gif', 'gif/30.gif', 'gif/31.gif', 'gif/32.gif', 'gif/33.gif', 'gif/34.gif', 'gif/35.gif', 'gif/36.gif', 'gif/37.gif', 'gif/38.gif', 'gif/39.gif', 'gif/40.gif', 'gif/41.gif', 'gif/42.gif', 'gif/43.gif', 'gif/44.gif', 'gif/45.gif', 'gif/46.gif', 'gif/47.gif', 'gif/48.gif', 'gif/49.gif', 'gif/50.gif', 'gif/51.gif', 'gif/52.gif', 'gif/53.gif', 'gif/54.gif', 'gif/55.gif', 'gif/56.gif', 'gif/57.gif', 'gif/58.gif'];
+
+var images = [];
 let randomImage;
 let randomImageDos;
 
@@ -17,6 +18,8 @@ let div, divWidth, divHeight, posX, posY;
 let imgW, imgH;
 
 function preload() {
+  images = ['gif/00.gif', 'gif/01.gif', 'gif/02.gif', 'gif/03.gif', 'gif/04.gif', 'gif/05.gif', 'gif/06.gif', 'gif/07.gif', 'gif/08.gif', 'gif/09.gif', 'gif/10.gif', 'gif/11.gif', 'gif/12.gif', 'gif/13.gif', 'gif/14.gif', 'gif/15.gif', 'gif/16.gif', 'gif/17.gif', 'gif/18.gif', 'gif/19.gif', 'gif/20.gif', 'gif/21.gif', 'gif/22.gif', 'gif/23.gif', 'gif/24.gif', 'gif/25.gif', 'gif/26.gif', 'gif/27.gif', 'gif/28.gif', 'gif/29.gif', 'gif/30.gif', 'gif/31.gif', 'gif/32.gif', 'gif/33.gif', 'gif/34.gif', 'gif/35.gif', 'gif/36.gif', 'gif/37.gif', 'gif/38.gif', 'gif/39.gif', 'gif/40.gif', 'gif/41.gif', 'gif/42.gif', 'gif/43.gif', 'gif/44.gif', 'gif/45.gif', 'gif/46.gif', 'gif/47.gif', 'gif/48.gif', 'gif/49.gif', 'gif/50.gif', 'gif/51.gif', 'gif/52.gif', 'gif/53.gif', 'gif/54.gif', 'gif/55.gif', 'gif/56.gif', 'gif/57.gif', 'gif/58.gif'];
+
   if (windowWidth > 768) {
     div = document.getElementById("anoesis");
     divWidth = div.clientWidth;
@@ -69,7 +72,7 @@ function draw() {
     bounce();
     image(img, pos.x, pos.y); //tracing frames
   } else if (imgW==0){
-    
+
   }
 }
 
@@ -109,6 +112,7 @@ function mouseClicked(){ // resets everything
 }
 
 function bounce() {
+  console.log(imgW, imgH);
   if (pos.x >= divWidth - imgW) { // bounce x-axis
     vel.x = -vel.x;
     pos.x = divWidth - imgW;
